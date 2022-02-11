286c3b9bc4bc2e2879998dbc9f4e59409fc5c3ce
chan289 <99498827+chan289@users.noreply.github.com>
2022-02-12 00:16:32 +0700

    Create chan1.java

---
+++ chan1.java
@@ -0,0 +1,14 @@
+public class chan1 {
+     public static void main(String[] args) {
+         sone e1=new chan();
+         e1. setName(chan);
+         e1. setAge(20);
+         e1. setyaer(3);
+         e1. setprintinofrmattione();
+         e1. setname();
+         e1. setgrade();
+         e1. setmark();
+         e1.setlocation();
+
+    }
+}
